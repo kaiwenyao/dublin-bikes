e6165fda588254c8613252392e2072582b0577a8
kaiwenyao <kaiwen.yao.cn@outlook.com>
2026-05-23 17:57:32 +0100

    Define PostgreSQL baseline schema

--- backend/src/main/java/dev/kaiwen/bikes/model/User.java
+++ backend/src/main/java/dev/kaiwen/bikes/model/User.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 
 @Entity
 @Table(
-        name = "user",
+        name = "users",
         uniqueConstraints = {
             @UniqueConstraint(columnNames = "username"),
             @UniqueConstraint(columnNames = "email"),
